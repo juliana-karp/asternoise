636276669c860ddb2c1f104a554c95c8b33cc120
Juliana Karp <116128279+juliana-karp@users.noreply.github.com>
2023-05-04 21:05:48 -0400

    make package pip installable

--- __init__.py
+++ __init__.py
@@ -5,3 +5,10 @@ Created on Mon Apr  3 11:12:09 2023
 @author: Juliana Karp
 """
 
+import numpy
+import astropy
+import streamlit
+import matplotlib
+
+from .add_noise import *
+from .visualize_noise import *
---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  4 20:02:48 2023
+
+@author: Juliana Karp
+"""
+
+import setuptools
+
+setuptools.setup(
+     name="asternoise",
+     version="0.1",
+     author="Juliana Karp",
+     author_email="juliana.karp@yale.edu",
+     description="a CCD noise visualization tool and teaching tool designed for Yale University's ASTR 255: Introduction to Astronomical Research course",
+     packages=["asternoise"],
+     python_requires='>=3'
+)
